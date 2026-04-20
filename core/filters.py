f2f73b8b6f02c8cbd3f601e966c5c66191bc91f9
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-19 20:41:35 -0500

    Update filters.py

--- core/filters.py
+++ core/filters.py
@@ -1,1 +1,21 @@
+def is_excluded(text, exclude_list):
+    text = text.lower()
+    return any(e in text for e in exclude_list)
 
+
+def domain_penalty(text):
+    text = text.lower()
+    penalty = 0
+
+    non_tech = [
+        "retail", "store", "sales",
+        "marketing", "hr", "finance",
+        "health", "nurse", "logistics",
+        "operations"
+    ]
+
+    for w in non_tech:
+        if w in text:
+            penalty -= 2
+
+    return penalty
